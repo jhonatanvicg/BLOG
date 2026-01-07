4389fbb076b34ca89d4cf531185f1c92a3fc5e82
jhonatanvictorioCertus <jhonatan.victorio@certuslab.com.mx>
2026-01-06 16:16:07 -0800

    Pequenas correcciones

---
+++ assets/js/3ec61122.96b4436e.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkblog=globalThis.webpackChunkblog||[]).push([[7586],{7677:l=>{l.exports=JSON.parse('{"metadata":{"permalink":"/BLOG/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.6dc7ca94.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkblog=globalThis.webpackChunkblog||[]).push([[1903],{1912:l=>{l.exports=JSON.parse('{"blogBasePath":"/BLOG/blog","blogTitle":"Blog","authorsListPath":"/BLOG/blog/authors"}')}}]);
